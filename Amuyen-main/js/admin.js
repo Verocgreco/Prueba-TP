fbc15932ac6f4782b703fdc434c07063294ca04a
martinyanov <131573985+martinyanov@users.noreply.github.com>
2023-11-28 23:52:39 -0300

    boton baja

--- Amuyen-main/js/admin.js
+++ Amuyen-main/js/admin.js
@@ -9,26 +9,27 @@ document.addEventListener("DOMContentLoaded", function () {
             const accesoBotones = '<div class="botones">'                
                 + '<button id="btnCarga"><h1>Carga de productos</h1></button>'
                 + '<button id="btnActualizar"><h1> Actualización de productos</h1></button>'
-                + '<button><h1>Baja de productos</h1></button></div>';
-
+                + '<button id="btnBaja"><h1>Baja de productos</h1></button></div>';
 
             acceso.innerHTML = accesoBotones;
             popup.innerHTML = '';
             const btnCarga = document.getElementById("btnCarga");
             const btnActualizar = document.getElementById("btnActualizar");
-            
+            const btnBaja = document.getElementById("btnBaja");
+
             btnCarga.addEventListener("click", function () {                
                 window.location.href = "carga.html";       
-
             });
+
             btnActualizar.addEventListener("click", function (){
-                window.location.href = "actualizacion.html"
+                window.location.href = "actualizacion.html";
             });
 
-
+            btnBaja.addEventListener("click", function (){  
+                window.location.href = "baja.html";
+            });
         } else {
             alert("Contraseña incorrecta")
         }
-
     })
 });
