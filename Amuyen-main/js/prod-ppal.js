55baa9cdaae9765ad7d45f4eac47d67bf2e9be57
Verocgreco <veronicacgreco@outlook.com>
2023-11-28 22:39:57 -0300

    arreglo de estilo en productos

--- Amuyen-main/js/prod-ppal.js
+++ Amuyen-main/js/prod-ppal.js
@@ -8,6 +8,7 @@ fetch(URL + 'productoss')
         for (let prod of data) {
             let cont = document.createElement('div');
             cont.classList.add('product-item');
+            cont.classList.add('product-details')
             cont.innerHTML = '<a href="prod-ind.html?codigo='+prod.codigo+'"><img src=/static/imagenes/' + prod.imagen_url + ' alt="Imagen del producto" ></a>'
                 + '<p>' + prod.nombre + '</p>';         
             contenedor[0].appendChild(cont);
